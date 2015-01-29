383c2a425510c7f5dcb26b60c4a96036ffee5ed5
Alexander Böhn <fish2000@gmail.com>
2015-01-29 15:09:12 -0500

    Olena Olverhaul

--- olena.rb
+++ olena.rb
@@ -15,6 +15,20 @@ class Olena < Formula
   
   option :cxx11
   option "with-scribo", "Enable Scribo Support (Whatever The F That Is)"
+  option "with-head-docs", "Try to make all the docs during a HEAD source-build"
+
+  head do
+    url 'https://github.com/glazzara/olena.git'
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "doxygen" => [:build, :optional] if build.with? "head-docs"
+    depends_on "hevea" => [:build, :optional] if build.with? "head-docs"
+    depends_on :tex => [:build, :optional] if build.with? "head-docs"
+  end
+
+  depends_on 'swig' => :recommended
+  depends_on :python => if not build.without? "swig"
   
   depends_on 'libtiff' => :recommended
   depends_on 'poppler' => :recommended
@@ -26,25 +40,49 @@ class Olena < Formula
 
   def install
     ENV.cxx11 if build.cxx11?
-    ENV['CPPFLAGS'] = "-I/usr/local/include/GraphicsMagick"
+    ENV['CPPFLAGS'] = "-I#{Formula['graphicsmagick'].opt_prefix}/include/GraphicsMagick"
     ENV['CPPFLAGS'] += " -DHAVE_SYS_TYPES_H=1"
     ENV['CXXFLAGS'] = "-fno-strict-aliasing"
     
-    cargs = [
-      "QT_PATH=/usr/local", "QMAKE=/usr/local/bin/qmake",
-                            "MOC=/usr/local/bin/moc", 
-                            "UIC=/usr/local/bin/uic",
-                            "RCC=/usr/local/bin/rcc",
-      "--with-graphicsmagickxx=#{Formula['graphicsmagick'].opt_prefix}",
-      "--with-imagemagickxx=no"]
+    # Hula hoop through which a brewer must jump to deal with python(s)
+    Language::Python.each_python(build) do |python, version|
+      
+      # Baseline arguments to .configure
+      cargs = [
+        "QT_PATH=#{Formula['qt'].opt_prefix}",
+        
+        "MOC=#{Formula['qt'].opt_prefix}/bin/moc", 
+        "UIC=#{Formula['qt'].opt_prefix}/bin/uic",
+        "RCC=#{Formula['qt'].opt_prefix}/bin/rcc",
+        "QMAKE=#{Formula['qt'].opt_prefix}/bin/qmake",
+        
+        "--with-graphicsmagickxx=#{Formula['graphicsmagick'].opt_prefix}",
+        "--with-imagemagickxx=no"]
+      
+      # Third-party imaging libraries
+      cargs << "--with-tiff=#{Formula['libtiff'].opt_prefix}" if not build.without? "libtiff"
+      cargs << "--with-tesseract=#{Formula['tesseract'].opt_prefix}" if not build.without? "tesseract"
+      
+      # Swig-generated Python bindings
+      if not build.without? "swig"
+        cargs << "--with-swig=#{Formula['swig'].opt_prefix}"
+        cargs << "--enable-swilena"
+        cargs << "PYTHON="+python
+      end
+      
+      # QT-backed Scribo UI
+      cargs << "--enable-scribo" if build.with? "scribo"
+      
+      # configure/make/install
+      system "./configure", "--prefix=#{prefix}", *cargs
+      system "make"
+      system "make install"
+      
+      # clean up logorrhea-inducing artifacts
+      (share/"doc/olena/milena/user-refman").rm_rf
+      
+    end # python enumeration thing
     
-    cargs << "--with-tiff=#{Formula['libtiff'].opt_prefix}" if not build.without? "libtiff"
-    cargs << "--with-tesseract=#{Formula['tesseract'].opt_prefix}" if not build.without? "tesseract"
-    cargs << "--enable-scribo" if build.with? "scribo"
-    
-    system "./configure", "--prefix=#{prefix}", *cargs
-    system "make" 
-    system "make install"
-    (share/"doc/olena/milena/user-refman").rm_rf
-  end
-end
+  end # install block is over
+  
+end # formula is over
