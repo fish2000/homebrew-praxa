e4480963f2c6c50bd2227e0e7f8550215921867e
Alexander Böhn <fish2000@gmail.com>
2015-01-29 11:27:05 -0500

    Overhauled the Olena formulaaaaa

--- olena.rb
+++ olena.rb
@@ -4,29 +4,48 @@
 
 class Olena < Formula
   homepage 'http://olena.lrde.epita.fr'
-  url 'http://www.lrde.epita.fr/dload/olena/2.0/olena-2.0a.tar.gz'
-  version "2.0"
-  sha1 'a9445bac1f30c9d999ad5ce70588745e153700dd'
-
-  depends_on 'qt'
-  depends_on 'fop'
+  
+  #url 'http://www.lrde.epita.fr/dload/olena/2.0/olena-2.0a.tar.gz'
+  #version "2.0"
+  #sha1 'a9445bac1f30c9d999ad5ce70588745e153700dd'
+  
+  url 'https://www.lrde.epita.fr/dload/olena/2.1/olena-2.1.tar.bz2'
+  version "2.1.0"
+  sha1 '54f756b033a45d4c2fe1233c10fc43f99f9f552f'
+  
+  option :cxx11
+  option "without-tesseract", "Disable Tesseract OCR Support"
+  option "without-tiff", "Disable TIFF Image Format Support"
+  option "with-scribo", "Enable Scribo Support (Whatever The F That Is)"
+  
   depends_on 'libxslt'
-  depends_on 'tesseract'
+  depends_on 'libtiff' => :optional
+  depends_on 'tesseract' => :optional
+  
   depends_on 'graphicsmagick'
+  depends_on 'fop'
+  depends_on 'qt'
 
   def install
-    system "./configure", "--prefix=#{prefix}",
-           "--enable-scribo", 
-           "QT_PATH=/usr/local", "QMAKE=/usr/local/bin/qmake",
-                                 "MOC=/usr/local/bin/moc", 
-                                 "UIC=/usr/local/bin/uic",
-                                 "RCC=/usr/local/bin/rcc",
-           "--with-tiff=/usr/local",
-           "--with-tesseract=/usr/local",
-           "--with-graphicsmagickxx=/usr/local", "--with-imagemagickxx=no",
-           "CPPFLAGS=-I/usr/local/include/GraphicsMagick -DHAVE_SYS_TYPES_H=1",
-           "CXXFLAGS=-fno-strict-aliasing"
+    ENV.cxx11 if build.cxx11?
+    ENV.append_to_cppflags "-I/usr/local/include/GraphicsMagick"
+    ENV.append_to_cppflags "-DHAVE_SYS_TYPES_H=1"
+    ENV.append_to_cxxflags "-fno-strict-aliasing"
+    #ENV.append_to_ldflags "-L /some/shit"
     
+    cargs = [
+      "QT_PATH=/usr/local", "QMAKE=/usr/local/bin/qmake",
+                            "MOC=/usr/local/bin/moc", 
+                            "UIC=/usr/local/bin/uic",
+                            "RCC=/usr/local/bin/rcc",
+      "--with-graphicsmagickxx=#{Formula['graphicsmagick'].opt_prefix}",
+      "--with-imagemagickxx=no"]
+    
+    cargs << "--with-tiff=#{Formula['libtiff'].opt_prefix}" if not build.without? "tiff"
+    cargs << "--with-tesseract=#{Formula['tesseract'].opt_prefix}" if not build.without? "tesseract"
+    cargs << "--enable-scribo" if build.with? "scribo"
+    
+    system "./configure", "--prefix=#{prefix}", *cargs
     system "make" 
     system "make install"
   end
