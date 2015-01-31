ed82f7be028133804e4571c445a2a0c5aff0591d
Alexander Böhn <fish2000@gmail.com>
2015-01-31 13:51:03 -0500

    Added a release download URL in vpp.rb

--- vpp.rb
+++ vpp.rb
@@ -1,10 +1,11 @@
 
 class Vpp < Formula
   homepage "https://github.com/matt-42/vpp"
+  url "https://github.com/matt-42/vpp/archive/0.2.tar.gz"
+  sha1 "b9f0a8a2bb25ecc16ca1b13b40cb6d62813350d6"
   head "https://github.com/matt-42/vpp.git", :using => :git
   
   option :cxx11
-  
   depends_on "cmake" => :build
   depends_on "eigen"
   depends_on "homebrew/science/opencv"
@@ -13,12 +14,14 @@ class Vpp < Formula
 
   def install
     ENV.cxx11 if build.cxx11?
-    
-    inreplace "CMakeLists.txt", "c++14", "c++1y"
     inreplace "CMakeLists.txt", "/usr/include/eigen3", "#{Formula['eigen'].opt_prefix}/include/eigen3"
-    #inreplace "examples/CMakeLists.txt", "c++14", "c++1y"
-    #inreplace "benchmarks/CMakeLists.txt", "c++14", "c++1y"
-    inreplace "tests/CMakeLists.txt", "c++14", "c++1y"
+    
+    if build.head?
+      inreplace "CMakeLists.txt", "c++14", "c++1y"
+      #inreplace "examples/CMakeLists.txt", "c++14", "c++1y"
+      #inreplace "benchmarks/CMakeLists.txt", "c++14", "c++1y"
+      inreplace "tests/CMakeLists.txt", "c++14", "c++1y"
+    end
     
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
