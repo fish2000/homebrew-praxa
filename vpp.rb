a96e42e16d2b009e07f5fbf3e9a09c625ccb0b66
Alexander Böhn <fish2000@gmail.com>
2015-02-03 09:09:31 -0500

    Using internal OpenCV formula

--- vpp.rb
+++ vpp.rb
@@ -7,9 +7,10 @@ class Vpp < Formula
   
   option :cxx11
   depends_on "cmake" => :build
+  depends_on "gcc" => :build
   #depends_on "llvm" => [:build, "clang"]
   depends_on "eigen"
-  depends_on "homebrew/science/opencv"
+  depends_on "fish2000/praxa/opencv"
   depends_on "fish2000/praxa/iod"
   depends_on "fish2000/praxa/dige"
 
@@ -17,17 +18,10 @@ class Vpp < Formula
     ENV.cxx11 if build.cxx11?
     inreplace "CMakeLists.txt", "/usr/include/eigen3", "#{Formula['eigen'].opt_prefix}/include/eigen3"
     
-    # if build.head?
-    #   inreplace "CMakeLists.txt", "c++14", "c++1y"
-    #   #inreplace "examples/CMakeLists.txt", "c++14", "c++1y"
-    #   #inreplace "benchmarks/CMakeLists.txt", "c++14", "c++1y"
-    #   inreplace "tests/CMakeLists.txt", "c++14", "c++1y"
-    # end
-    
     mkdir "build" do
-      llvm_pth = Formula['llvm'].opt_prefix
-      ENV['CC'] = Formula['llvm'].opt_prefix/"bin/clang"
-      ENV['CXX'] = Formula['llvm'].opt_prefix/"bin/clang++"
+      gccpth = Formula['gcc'].opt_prefix
+      ENV['CC'] = gccpth+"bin/gcc-4.9"
+      ENV['CXX'] = gccpth+"bin/g++-4.9"
       system "cmake", "..", *std_cmake_args
       system "make"
       system "make", "install"
