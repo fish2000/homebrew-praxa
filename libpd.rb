04f50d44b07902702ce6340de78544dae5c2ea8a
Alexander Böhn <fish2000@gmail.com>
2015-02-03 18:17:10 -0500

    Functional libpd/pylibpd install

--- libpd.rb
+++ libpd.rb
@@ -3,34 +3,39 @@ class Libpd < Formula
   homepage "http://libpd.cc/"
   head "https://github.com/libpd/libpd.git"
   
+  depends_on :python => :recommended
+  depends_on "swig"  => :recommended
+  
   def install
-    # ENV['CC'] = Formula['llvm'].opt_prefix/"bin/clang"
-    # ENV['CXX'] = Formula['llvm'].opt_prefix/"bin/clang++"
-    ENV['CC'] = "/usr/bin/clang"
-    ENV['CXX'] = "/usr/bin/clang++"
+    # Fix hardcoded gcc/g++ refs in the Makefile
+    inreplace "Makefile", "gcc -o", "$(CC) -o"
+    inreplace "Makefile", "g++ -o", "$(CXX) -o"
     
-    # system "git", "submodule", "init"
-    # system "git", "submodule", "update"
-    system "make", "UTIL=true", "EXTRA=true",
-                   "CFLAGS=-Wno-parentheses -Wno-implicit-function-declaration"
+    # Build the library
+    system "make", "UTIL=true", "EXTRA=true"
+    #system "make", "cpplib", "UTIL=true", "EXTRA=true"
     
+    # Build and install the python module
+    if build.with? "python" and build.with? "swig"
+      cd "python" do
+        ENV.prepend_create_path "PYTHONPATH", lib/"python2.7/site-packages"
+        system "python", "setup.py", "build_ext", "--swig", Formula['swig'].opt_prefix/"bin/swig"
+        system "python", "setup.py", "install", "--prefix=#{prefix}"
+      end
+    end
+    
+    # Install the library and ancilliaries
+    prefix.install %w(LICENSE.txt README.md)
     lib.mkpath
     lib.install "libs/libpd.dylib"
     lib.install "dist/libpd.jar"
-    
-    prefix.install %w(LICENSE.txt README.md)
-    (prefix/"objc").mkpath
-    (prefix/"objc").install Dir["objc/*"]
-    
-    cd "python" do
-      ENV.prepend_create_path "PYTHONPATH", lib/"python2.7/site-packages"
-      system "python", "setup.py", "install", "--prefix=#{prefix}"
-    end
-    
-    # cd "pure-data" do
-    #   system "autogen.sh"
-    #   system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking", "--enable-fftw"
-    # end
+    include.mkpath
+    include.install Dir["libpd_wrapper/*.h"]
+    include.install "pure-data/src/m_pd.h"
+    (include/"util").mkpath
+    (include/"util").install Dir["libpd_wrapper/util/*.h"]
+    (share/"objc").mkpath
+    (share/"objc").install Dir["objc/*"]
     
   end
 
