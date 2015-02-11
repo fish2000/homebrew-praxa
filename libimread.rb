6fec9224b29b8dd8ee233039f4aba76edae34b12
Alexander Böhn <fish2000@gmail.com>
2015-02-10 19:08:48 -0500

    Formula for libimread - https://github.com/fish2000/libimread

---
+++ libimread.rb
@@ -0,0 +1,24 @@
+
+class Libimread < Formula
+  homepage "https://github.com/fish2000/libimread"
+  head "https://github.com/fish2000/libimread.git", :using => :git
+  
+  depends_on "cmake" => :build
+  depends_on "llvm"  => :build
+  depends_on "libjpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "webp"
+  
+  def install
+    # Use brewed clang
+    ENV['CC'] = Formula['llvm'].opt_prefix/"bin/clang"
+    ENV['CXX'] = Formula['llvm'].opt_prefix/"bin/clang++"
+    
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+    
+  end
+end
