e46f31c65068aa1f1baa5e12c45526f2aa2609ff
Alexander Böhn <fish2000@gmail.com>
2015-02-26 04:47:47 -0500

    Worked around recent IOD build system overhauls

--- iod.rb
+++ iod.rb
@@ -1,8 +1,11 @@
 
 class Iod < Formula
   homepage "https://github.com/matt-42/iod"
-  url "https://github.com/matt-42/iod.git", :using => :git
-  version "0.1.0"
+  #url "https://github.com/matt-42/iod/archive/master.zip"
+  url "https://github.com/matt-42/iod/archive/fd241227b33aa1f0c9ae80a384aa2bb7f7e17609.zip"
+  head "https://github.com/matt-42/iod.git",
+    :using => :git
+  version "0.6.6"
   
   depends_on "cmake" => :build
   depends_on "llvm"  => :build
