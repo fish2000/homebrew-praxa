77a8b504b98de3fad2fc7264dfed8b22a7d01596
Alexander Böhn <fish2000@gmail.com>
2015-04-07 20:35:12 -0400

    Oyranos formula tweaks

--- oyranos.rb
+++ oyranos.rb
@@ -5,9 +5,12 @@ class Oyranos < Formula
   
   depends_on "cmake" => :build
   depends_on "llvm"  => :build
-  depends_on "gettext" => :build
+  depends_on "doxygen" => :build
+  depends_on "graphviz" => :build
   depends_on "qt" => :recommended
+  depends_on "gettext"
   depends_on "libpng"
+  depends_on "intltool"
   depends_on "fltk"
   depends_on "exiv2"
   depends_on "cairo"
@@ -21,9 +24,17 @@ class Oyranos < Formula
     # Use brewed clang
     ENV['CC'] = Formula['llvm'].opt_prefix/"bin/clang"
     ENV['CXX'] = Formula['llvm'].opt_prefix/"bin/clang++"
+    # ENV['CFLAGS'] = "-I#{Formula['gettext'].opt_prefix}/include"
+    # ENV['CPPFLAGS'] = "-I#{Formula['gettext'].opt_prefix}/include"
+    # ENV['CXXFLAGS'] = "-I#{Formula['gettext'].opt_prefix}/include"
+    
+    cargs = std_cmake_args + %W[
+      -DCMAKE_INCLUDE_PATH=#{Formula['gettext'].opt_prefix}/include:/usr/include/libxml2:/opt/X11/include:/opt/X11/include/freetype2:/System/Library/Frameworks/OpenGL.framework/Versions/Current/Headers
+      -DCMAKE_LIBRARY_PATH=#{Formula['gettext'].opt_prefix}/lib:/opt/X11/lib:/System/Library/Frameworks/OpenGL.framework/Versions/Current/Libraries
+    ]
     
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args
+      system "cmake", "..", *cargs
       system "make", "install"
     end
     
