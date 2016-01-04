895a63db143b50bcb3afb4a8dfad5e4be92ee0f2
Alexander Böhn <fish2000@gmail.com>
2016-01-04 12:29:56 -0500

    Bumped HipText version

--- hiptext.rb
+++ hiptext.rb
@@ -2,11 +2,10 @@
 class Hiptext < Formula
   desc "Turn images into text better than caca/aalib"
   homepage "https://github.com/fish2000/hiptext"
-  url "https://github.com/fish2000/hiptext/archive/0.1.1.zip"
-  sha256 "15e5cd151d3531dd497a1065ac0b9384d6fc5f50f21b261b2c497b32c2a3ad3f"
+  url "https://github.com/fish2000/hiptext/archive/0.5.0.zip"
+  sha256 "8f704b1e4ba8e641a8b31ec669d6802c5819d04a952db8e078d0c0de6e27a89d"
   head "https://github.com/fish2000/hiptext.git", :using => :git
   
-  # depends_on "llvm" => :build
   depends_on "cmake" => :build
   depends_on "ragel"  => :build
   depends_on "gflags"
@@ -19,11 +18,7 @@ class Hiptext < Formula
   depends_on "zlib"
   
   def install
-    # Use brewed clang
-    # ENV['CC'] = Formula['llvm'].opt_prefix/"bin/clang"
-    # ENV['CXX'] = Formula['llvm'].opt_prefix/"bin/clang++"
     ENV.cxx11
-    
     cargs = std_cmake_args
     cargs.keep_if { |v| v !~ /DCMAKE_VERBOSE_MAKEFILE/ }
     
