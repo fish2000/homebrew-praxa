7a922e073185a0f66ddf85e92287995c78512ac4
Alexander Böhn <fish2000@gmail.com>
2016-01-18 17:22:58 -0500

    Python binding support in the libimread formula

--- libimread.rb
+++ libimread.rb
@@ -4,17 +4,27 @@ class Libimread < Formula
   url "https://github.com/fish2000/libimread.git", :using => :git
   version "0.1.0"
   
-  depends_on "cmake" => :build
-  depends_on "llvm"  => :build
+  option "with-brewed-clang",
+         "Compile using Clang from Homebrew-installed LLVM package"
+  
+  depends_on "cmake"          =>  :build
+  depends_on "llvm"           =>  :build
+  depends_on "pkg-config"     =>  :build
+  depends_on "iod-symbolizer" => [:python, :build]
+  depends_on :python          =>  :optional
+  depends_on "hdf5"
   depends_on "libjpeg"
   depends_on "libpng"
   depends_on "libtiff"
   depends_on "webp"
+  depends_on "zlib"
   
   def install
     # Use brewed clang
-    # ENV['CC'] = Formula['llvm'].opt_prefix/"bin/clang"
-    # ENV['CXX'] = Formula['llvm'].opt_prefix/"bin/clang++"
+    if build.with? "brewed-clang"
+      ENV['CC'] = Formula['llvm'].opt_prefix/"bin/clang"
+      ENV['CXX'] = Formula['llvm'].opt_prefix/"bin/clang++"
+    end
     ENV.cxx11
     
     cargs = std_cmake_args
@@ -23,6 +33,20 @@ class Libimread < Formula
     mkdir "build" do
       system "cmake", "..", *cargs
       system "make", "install"
+    end
+    
+    if build.with? :python
+      cd "python" do
+        bin.mkdir
+        lib.mkdir
+        pylib = lib/"python2.7/site-packages"
+        ENV.prepend_create_path "PYTHONPATH", pylib
+        system "python", "setup.py", "build"
+        system "python", "setup.py", "install",
+                         "--prefix=#{prefix}",
+                         "--install-scripts=#{bin}",
+                         "--install-lib=#{pylib}"
+      end
     end
     
   end
