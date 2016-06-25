58a132ec4cfbdf6505d0443a25dfce745e2bfc24
Alexander Böhn <fish2000@gmail.com>
2016-06-25 18:05:47 -0400

    CLeaned up VPP formula

--- vpp.rb
+++ vpp.rb
@@ -5,26 +5,22 @@ class Vpp < Formula
   url "https://github.com/matt-42/vpp/archive/e03f0dd4d33953c0017b946995960e491a928426.zip"
   head "https://github.com/matt-42/vpp.git",
        :using => :git
-  version "0.2.0"
+  version "0.3.0"
   
   option "with-benchmarks", "Build and run benchmarks"
-  option "with-examples", "Build and install examples"
+  option "with-examples",   "Build and install examples"
   
   depends_on "cmake" => :build
-  depends_on "llvm" => :build
+  depends_on "llvm"  => :build
   depends_on "eigen"
   depends_on "homebrew/science/opencv"
   depends_on "fish2000/praxa/iod"
   depends_on "fish2000/praxa/dige"
   
-  # Fix the CMake build system
-  #patch :DATA if build.head?
-  patch :DATA
-  
   def install
     # Use brewed Clang
-    ENV['CC'] = Formula['llvm'].opt_prefix/"bin/clang"
-    ENV['CXX'] = Formula['llvm'].opt_prefix/"bin/clang++"
+    # ENV['CC'] = Formula['llvm'].opt_prefix/"bin/clang"
+    # ENV['CXX'] = Formula['llvm'].opt_prefix/"bin/clang++"
     
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
@@ -53,193 +49,3 @@ class Vpp < Formula
     
   end
 end
-
-__END__
-From 41ee214c09a851fdc65c70923274c661dd66f727 Mon Sep 17 00:00:00 2001
-From: =?UTF-8?q?Alexander=20Bo=CC=88hn?= <fish2000@gmail.com>
-Date: Tue, 3 Feb 2015 15:21:47 -0500
-Subject: [PATCH] Updates
-
----
- CMakeLists.txt            |  2 +-
- benchmarks/CMakeLists.txt | 24 ++++++++----------------
- examples/CMakeLists.txt   | 12 ++++++------
- examples/pyrlk.cc         |  1 +
- tests/CMakeLists.txt      | 13 +++----------
- tests/pyrlk.cc            |  3 ++-
- 6 files changed, 21 insertions(+), 34 deletions(-)
-
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index ca1d1a0..5b131b5 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -2,7 +2,7 @@ cmake_minimum_required (VERSION 2.8)
- 
- project (vpp)
- 
--include_directories(/usr/include/eigen3)
-+include_directories(/usr/local/opt/eigen/include/eigen3)
- 
- add_definitions(-std=c++14)
- add_subdirectory(tests)
-diff --git a/benchmarks/CMakeLists.txt b/benchmarks/CMakeLists.txt
-index e6fc715..44cf76b 100644
---- a/benchmarks/CMakeLists.txt
-+++ b/benchmarks/CMakeLists.txt
-@@ -3,12 +3,9 @@ project (vpp_benchmarks)
- 
- find_package(OpenCV REQUIRED)
- 
--include_directories(/usr/include/eigen3  $ENV{HOME}/projects/iod)
-+include_directories(/usr/local/opt/eigen/include/eigen3 ${OpenCV_INCLUDE_DIRS})
- include_directories(${CMAKE_CURRENT_SOURCE_DIR}/..)
--
--#add_definitions(-O3 -std=c++1y -march=native -DNDEBUG -fopenmp -fprofile-use=./CMakeFiles/distance_transform.dir/distance_transform.cc.gcda)
--add_definitions(-O3 -std=c++1y -march=native -DNDEBUG -fopenmp)
--#add_definitions(-O3 -fopenmp -fno-tree-vectorize -std=c++1y -march=native -DNDEBUG -mno-avx -mno-avx2 -mno-sse4)
-+add_definitions(-O3 -std=c++14 -DNDEBUG)
- 
- set(CMAKE_VERBOSE_MAKEFILE ON)
- add_executable(boxNd_iterator boxNd_iterator.cc)
-@@ -25,14 +22,9 @@ add_executable(liie liie.cc)
- add_executable(distance_transform distance_transform.cc)
- add_executable(lbp lbp.cc)
- 
--target_link_libraries(image_iterations gomp)
--target_link_libraries(image_add gomp opencv_core)
--target_link_libraries(box_filter gomp)
--target_link_libraries(box_5x5_filter gomp opencv_core opencv_imgproc)
--target_link_libraries(box2d_filter gomp)
--target_link_libraries(fast_detector ${OpenCV_LIBS}  gomp)
--target_link_libraries(for_fast ${OpenCV_LIBS}  gomp)
--target_link_libraries(pyrlk_opencv_comparison ${OpenCV_LIBS} gomp)
--target_link_libraries(liie gomp)
--target_link_libraries(distance_transform gomp ${OpenCV_LIBS} gcov)
--target_link_libraries(lbp gomp)
-+target_link_libraries(image_add opencv_core)
-+target_link_libraries(box_5x5_filter opencv_core opencv_imgproc)
-+target_link_libraries(fast_detector ${OpenCV_LIBS})
-+target_link_libraries(for_fast ${OpenCV_LIBS})
-+target_link_libraries(pyrlk_opencv_comparison ${OpenCV_LIBS})
-+target_link_libraries(distance_transform ${OpenCV_LIBS} gcov)
-diff --git a/examples/CMakeLists.txt b/examples/CMakeLists.txt
-index 79dc196..f0a1009 100644
---- a/examples/CMakeLists.txt
-+++ b/examples/CMakeLists.txt
-@@ -27,18 +27,18 @@ set(LIBS
- 
- link_directories(${THIRDPARTY}/lib)
- 
--add_definitions(-std=c++1y -g -fopenmp)
--add_definitions(-Ofast -march=native)
-+add_definitions(-std=c++14 -g)
-+add_definitions(-Ofast)
- add_definitions(-DNDEBUG)
- 
- include_directories(${CMAKE_CURRENT_SOURCE_DIR}/..)
--include_directories(/usr/include/eigen3)
-+include_directories(/usr/local/opt/eigen/include/eigen3)
- 
- add_executable(box_filter box_filter.cc)
--target_link_libraries(box_filter ${OpenCV_LIBS} gomp)
-+target_link_libraries(box_filter ${OpenCV_LIBS})
- 
- add_executable(fast_detector fast_detector.cc)
--target_link_libraries(fast_detector ${OpenCV_LIBS} gomp)
-+target_link_libraries(fast_detector ${OpenCV_LIBS})
- 
- add_executable(pyrlk pyrlk.cc)
--target_link_libraries(pyrlk ${OpenCV_LIBS} ${LIBS} gomp)
-+target_link_libraries(pyrlk opencv_highgui ${OpenCV_LIBS} ${LIBS})
-diff --git a/examples/pyrlk.cc b/examples/pyrlk.cc
-index bf950de..1f3ffed 100644
---- a/examples/pyrlk.cc
-+++ b/examples/pyrlk.cc
-@@ -1,4 +1,5 @@
- #include <iostream>
-+#include <opencv2/opencv.hpp>
- #include <opencv2/highgui/highgui.hpp>
- 
- #include <vpp/vpp.hh>
-diff --git a/tests/CMakeLists.txt b/tests/CMakeLists.txt
-index 59aab69..056a206 100644
---- a/tests/CMakeLists.txt
-+++ b/tests/CMakeLists.txt
-@@ -4,7 +4,7 @@ find_package(OpenCV REQUIRED)
- 
- enable_testing()
- 
--include_directories(${CMAKE_CURRENT_SOURCE_DIR}/.. /usr/include/eigen3 $ENV{HOME}/projects/iod)
-+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/.. ${OpenCV_INCLUDE_DIRS} /usr/local/opt/eigen/include/eigen3)
- add_definitions(-std=c++14)
- 
- add_executable(imageNd imageNd.cc)
-@@ -23,7 +23,6 @@ add_executable(imageNd_iterator imageNd_iterator.cc)
- add_test(imageNd_iterator imageNd_iterator)
- 
- add_executable(pixel_wise pixel_wise.cc)
--target_link_libraries(pixel_wise gomp)
- add_test(pixel_wise pixel_wise)
- 
- add_executable(box_nbh2d box_nbh2d.cc)
-@@ -33,10 +32,10 @@ add_executable(sandbox sandbox.cc)
- add_test(sandbox sandbox)
- 
- add_executable(window window.cc)
--target_link_libraries(window gomp)
- add_test(window window)
- 
- add_executable(opencv_bridge opencv_bridge.cc)
-+target_link_libraries(opencv_bridge ${OpenCV_LIBS})
- add_test(opencv_bridge opencv_bridge)
- 
- add_executable(fill fill.cc)
-@@ -55,25 +54,19 @@ add_executable(tuple_utils tuple_utils.cc)
- add_test(tuple_utils tuple_utils)
- 
- add_executable(block_wise block_wise.cc)
--target_link_libraries(block_wise gomp)
- add_test(block_wise block_wise)
- 
--
- add_executable(colorspace_conversions colorspace_conversions.cc)
- add_test(colorspace_conversions colorspace_conversions)
- 
- add_executable(pyrlk pyrlk.cc)
-+target_link_libraries(pyrlk ${OpenCV_LIBS})
- add_test(pyrlk pyrlk)
- 
--
- add_executable(liie liie.cc)
- add_test(liie liie)
- 
--
- add_executable(lbp lbp.cc)
- add_test(lbp lbp)
- 
--target_link_libraries(pyrlk ${OpenCV_LIBS})
--target_link_libraries(opencv_bridge ${OpenCV_LIBS})
--target_link_libraries(liie gomp)
- 
-diff --git a/tests/pyrlk.cc b/tests/pyrlk.cc
-index ded86e2..8ec80c1 100644
---- a/tests/pyrlk.cc
-+++ b/tests/pyrlk.cc
-@@ -6,6 +6,7 @@
- #include <vpp/algorithms/filters/scharr.hh>
- 
- #include <opencv2/opencv.hpp>
-+#include <opencv2/highgui/highgui.hpp>
- 
- using namespace vpp;
- 
-@@ -67,5 +68,5 @@ int main(int argc, char* argv[])
- 
-   assert(keypoints.size() == 1);
-   std::cout << keypoints[0].position.transpose() << std::endl;
--  assert((keypoints[0].position - vfloat2(52,52)).norm() < 0.2);
-+  //assert((keypoints[0].position - vfloat2(52,52)).norm() < 0.2);
- }
--- 
-2.2.2
-
